be8abdee650054d223da25196346595372924e03
Twahidul Islam <twahidulislam2005@gmail.com>
2026-06-05 12:28:06 +0600

    refactor: remove unused SVG files and update Header component with logo and navigation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Home Page</h1>
+    <h1 className="text-3xl text-center font-bold underline">Home Page</h1>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Container from "./Container";
 import Flex from "./Flex";
+import Image from "next/image";
+import BrandLogo from "../../public/brandLogo.png";
 
 const Header = () => {
   return (
-    <div className="bg-gray-800 text-white">
+    <div className="">
       <Container>
         <Flex className="justify-between items-center py-4">
-          <h1 className="text-3xl font-bold ">Go Trip</h1>
-          <ul className="flex space-x-4">
-            <li>Home</li>
-            <li>Destinations</li>
-            <li>Contact</li>
+          <div className="">
+            <Image src={BrandLogo} alt="Go Trip Logo" width={100} height={50} />
+          </div>
+          <ul className="flex space-x-10 text-black">
+            <li>Tour Packages</li>
+            <li>Hotel</li>
+            <li>Activity</li>
+            <li>Flight</li>
+            <li>Car Rental</li>
           </ul>
+          <div className="flex items-center space-x-4">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Sign In
+            </button>
+          </div>
         </Flex>
       </Container>
     </div>
